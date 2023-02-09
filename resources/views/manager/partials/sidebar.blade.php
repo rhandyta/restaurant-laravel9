353bb182f3575a7fa6cb1d9e2c00d625001f95cd
Rhandyta Briantama <rhandytabriantama@gmail.com>
2023-02-09 23:33:49 +0700

    icon menu

--- resources/views/manager/partials/sidebar.blade.php
+++ resources/views/manager/partials/sidebar.blade.php
@@ -10,7 +10,7 @@
                 @if($menu->submenus->count() < 1)
                     <li class="sidebar-item {{request()->path() == substr($menu->path, 1) ? 'active' : null}}">
                         <a href="{{$menu->path}}" class='sidebar-link'>
-                            <i class="bi bi-grid-fill"></i>
+                            <i class="bi bi-{{$menu->icon}}"></i>
                             <span>{{$menu->label_menu}}</span>
                         </a>
                     </li>
@@ -18,7 +18,7 @@
                 @if ($menu->submenus->count() > 0)
                     <li class="sidebar-item has-sub {{request()->path() == substr($menu->path, 1) ? 'active' : null}}">
                         <a href="{{$menu->path}}" class='sidebar-link'>
-                            <i class="bi bi-journal-check"></i>
+                            <i class="bi bi-{{$menu->icon}}"></i>
                             <span>{{$menu->label_menu}}</span>
                         </a>
                         <ul class="submenu">
